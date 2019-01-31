227f735668aa7b59ac170c9913e0aacab82e9886
Vanna Vu <vannavu5539@gmail.com>
2019-01-30 22:33:52 -0600

    initial commit

---
+++ _site/js/script.js
@@ -0,0 +1,240 @@
+$(document).ready(function() {
+
+	$("#click").hover(
+  function() {
+    $("#hover_gallery").css({
+			"background": "url(images/index/click.gif)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"transition": "background 0.5s"
+		});
+  }, function() {
+		$("#hover_gallery").css({
+			"background": "url(images/index/click.gif)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+		});
+  }
+	);
+
+	$("#scroll").hover(
+  function() {
+    $("#hover_gallery").css({
+			"background": "url(images/index/scroll.gif)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"transition": "background 0.5s"
+		});
+  }, function() {
+		$("#hover_gallery").css({
+			"background": "url(images/index/scroll.gif)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+		});
+  }
+	);
+
+	$("#resize").hover(
+  function() {
+    $("#hover_gallery").css({
+			"background": "url(images/index/resize.gif)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"transition": "background 0.5s"
+		});
+  }, function() {
+		$("#hover_gallery").css({
+			"background": "url(images/index/resize.gif)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+		});
+  }
+	);
+
+	$("#week1").hover(
+  function() {
+    $("body").css({
+			"background": "url(images/index/1.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week2").hover(
+  function() {
+    $("body").css({
+			"background": "url(images/index/2.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week3").hover(
+  function() {
+    $("body").css({
+			"background": "url(images/index/3.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week4").hover(
+  function() {
+    $("body").css({
+			"background": "url(images/index/4.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week5").hover(
+  function() {
+    $("body").css({
+			"background": "url(images/index/5.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week6").hover(
+	function() {
+		$("body").css({
+			"background": "url(images/index/6.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week7").hover(
+	function() {
+		$("body").css({
+			"background": "url(images/index/7.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week9").hover(
+	function() {
+		$("body").css({
+			"background": "url(images/index/9.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week10").hover(
+	function() {
+		$("body").css({
+			"background": "url(images/index/10.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week11").hover(
+	function() {
+		$("body").css({
+			"background": "url(images/index/11.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week12").hover(
+	function() {
+		$("body").css({
+			"background": "url(images/index/12.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+	$("#week14").hover(
+	function() {
+		$("body").css({
+			"background": "url(images/index/14.png)",
+			"background-repeat": "no-repeat",
+			"background-position": "center",
+			"background-size": "cover",
+			"background-attachment": "fixed",
+		});
+	}, function() {
+		$("body").css({
+			"background": "url()"
+		});
+	});
+
+});
---
+++ index.md
@@ -0,0 +1,5 @@
+---
+title: Capstone Process
+layout: home
+---
+# <span id="title">Capstone Process</span><span id="date">Spring 2019</span>
